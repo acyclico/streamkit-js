8da6b9b194087e3a5b65c6a1bdd4ed33900cc101
Juan A. Fernandez <juan.fdezjimenez@gmail.com>
2019-07-27 01:02:17 +0200

    Example to retrieve pollution levels in Madrid (Spain)

---
+++ examples/pollution/src/App.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import streamkit from 'streamkit-js';
+
+import './App.css';
+import regions from './regions';
+import magnitudes from './magnitudes';
+
+
+class App extends React.Component {
+  static defaultProps = {
+    center: {
+      lat: 40.416775,
+      lng: -3.703790
+    },
+    zoom: 13
+  };
+  
+  state = {
+    measures: {},
+    hour: 0,
+    magnitude: "08"
+  }
+
+  constructor(props) {
+    super(props);
+    this.sk = streamkit();
+  }
+
+  async componentDidMount() {
+    const measures = {};
+    const stream = await this.sk.stream('mad_env');
+    stream
+      .events()
+      .subscribe(event => {
+        const datas = event.mad_env.data.split('\n')
+              .map(data => data.split(','));
+        
+        datas.forEach(data => {
+          const regionId = data[0] + data[1] + data[2];
+          const region = regions[regionId];
+          const magnitudeId = data[3];
+          const dataMeasures = data.splice(9);
+
+          dataMeasures.forEach((measure, idx) => {
+            if (idx % 2 === 1) return;
+
+            const mIdx = idx / 2;
+            
+            if (measures[magnitudeId] === undefined)
+              measures[magnitudeId] = [];
+            if (measures[magnitudeId][mIdx] === undefined)
+              measures[magnitudeId][mIdx] = [];
+
+            measures[magnitudeId][mIdx].push({
+              ...region,
+              weight: parseInt(measure, 10)
+            });            
+          });
+        });
+
+        this.setState({ measures: measures });
+      });
+  }
+
+  handleHourChange = (event) => {
+    this.setState({ hour: event.target.value });
+  }
+
+  handleMagnitudeChange = (event) => {
+    this.setState({ magnitude: event.target.value });
+  }
+  
+  render() {
+    const magnitude = this.state.magnitude;
+    const measures = this.state.measures[magnitude]
+                   ? this.state.measures[magnitude][this.state.hour]
+                   : [];
+
+    let maxIntensity = magnitude && this.state.measures[magnitude]
+                     ? Math.max(
+                       ...this.state.measures[magnitude].map(
+                         measures => Math.max(...measures.map(m => m.weight))
+                       )
+                     )
+                     : 0;
+    
+    const heatMapData = {
+  		positions: measures,
+		  options: {
+			  radius: 100,
+			  opacity: 0.7,
+        dissipating: true,
+        maxIntensity: maxIntensity
+		  }
+    };
+
+    const magnitudeOptions = Object.keys(magnitudes).map(
+      (magnitude, idx) => (
+        <option key={magnitude} value={magnitude}>
+          { magnitudes[magnitude] }
+        </option>
+      )
+    );
+
+    if (measures.length === 0)
+      return (<div></div>);
+    else
+      return (
+      <div style={{ display: 'flex' }}>
+        <div style={{ height: '100vh', width: '75vw' }}>
+          <GoogleMapReact
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+            heatmapLibrary={true}
+            heatmap={heatMapData}
+            >
+          </GoogleMapReact>
+        </div>
+        <div className="magnitudeDetails">
+          <select value={magnitude}
+                  onChange={this.handleMagnitudeChange}
+                  className="magnitudeSelector"
+          >
+            { magnitudeOptions }
+          </select>
+          <div className="hourSelector">
+            <input type="range"
+                   name="hour"
+                   value={this.state.hour}
+                   min="0"
+                   max="23"
+                   onChange={this.handleHourChange}
+            />
+            <label className="label">
+              {this.state.hour} Hour
+            </label>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ examples/pollution/src/regions.js
@@ -0,0 +1,54 @@
+module.exports = {
+  28079001: { name: 'Recoletos', lat: 40.424173, lng: -3.685487 },
+  28079002: { name: 'Glorieta de Carlos V', lat: 40.407169, lng: -3.693400 },
+  28079003: { name: 'Plaza del Carmen', lat: 40.418700, lng: -3.703717 },
+  28079035: { name: 'Plaza del Carmen', lat: 40.418700, lng: -3.703717 },
+
+  28079004: { name: 'Plaza de España', lat: 40.423531, lng: -3.713290 },
+  28079005: { name: 'Barrio del Pilar', lat: 40.477047, lng: -3.703539 },
+  28079039: { name: 'Barrio del Pilar', lat: 40.477047, lng: -3.703539 },
+  
+  28079006: { name: 'Plaza Dr Marañón', lat: 40.438210, lng: -3.690926 },
+  28079007: { name: 'Plaza de M. De Salamanca', lat: 40.430412, lng: -3.679505 },
+  28079008: { name: 'Escuelas Aguirre', lat: 40.421704, lng: -3.681734 },
+  28079009: { name: 'Plaza Luca de Tena', lat: 40.447770, lng: -3.631823 },
+  28079010: { name: 'Cuatro Caminos', lat: 40.446812, lng: -3.703981 },
+  28079038: { name: 'Cuatro Caminos', lat: 40.446812, lng: -3.703981 },
+
+  28079011: { name: 'Av. Ramón y Cajal', lat: 40.451599, lng: -3.674298},
+  28079012: { name: 'Plaza Manuel Becerra', lat: 40.428078, lng: -3.668211},
+  28079013: { name: 'Vallecas', lat: 40.386860, lng: -3.659180 },
+  28079040: { name: 'Vallecas', lat: 40.386860, lng: -3.659180 },
+
+  28079014: { name: 'Plaza Fernández Ladreda', lat: 40.385615, lng: -3.717125 },
+  28079015: { name: 'Plaza Castilla', lat: 40.466282, lng: -3.689664 },
+
+  28079016: { name: 'Arturo Soria', lat: 40.455936, lng: -3.656177 },
+  28079017: { name: 'Villaverde Alto', lat: 40.341517, lng: -3.711587 },
+  28079018: { name: 'Farolillo', lat: 40.394551, lng: -3.732263 },
+  28079019: { name: 'Huerta Castañeda', lat: 40.408016, lng: -3.741582 },
+  28079020: { name: 'Moratalaz', lat: 40.405521, lng: -3.640157 },
+  28079036: { name: 'Moratalaz', lat: 40.405521, lng: -3.640157 },
+  
+  28079021: { name: 'Plaza Cristo Rey', lat: 40.439706 , lng: -3.717934},
+  28079022: { name: 'Pontones', lat: 40.404041, lng: -3.720167 },
+  28079023: { name: 'Final C/Alcalá', lat: 40.449113, lng: -3.608586 },
+
+  28079024: { name: 'Casa de Campo', lat: 40.403529, lng: -3.760889 },
+  28079025: { name: 'Santa Eugenia', lat: 40.382924, lng: -3.611767 },
+  28079026: { name: 'Urb. Embajada (Barajas)', lat: 40.485988, lng: -3.613864 },
+  28079027: { name: 'Barajas', lat: 40.479840, lng: -3.580251 },
+  28079047: { name: 'Méndez Alvaro', lat: 40.395603, lng: -3.678593 },
+  28079048: { name: 'Castellana', lat: 40.433823, lng: -3.684004 },
+  28079049: { name: 'Retiro', lat: 40.415459, lng: -3.684400 },
+  28079050: { name: 'Plaza Castilla', lat: 40.466282, lng: -3.689664},
+  28079054: { name: 'Ensanche Vallecas', lat: 40.364976, lng: -3.599905 },
+  28079055: { name: 'Urb. Embajada', lat: 40.485988, lng: -3.613864 },
+  28079056: { name: 'Plaza Elíptica', lat: 40.385356, lng: -3.717875 },
+  28079057: { name: 'Sanchinarro', lat: 40.493189, lng: -3.656402},
+  28079058: { name: 'El pardo', lat: 40.521303, lng: -3.775063 },
+  28079059: { name: 'Parque Juan Carlos I', lat: 40.462616, lng: -3.605402},
+  28079086: { name: 'Tres Olivos', lat: 40.500951, lng: -3.694627},
+  28079060: { name: 'Tres Olivos', lat: 40.500951, lng: -3.694627 }
+};
+
